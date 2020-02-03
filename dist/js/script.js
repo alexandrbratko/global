88df6865ce43ae271f6a8de17671115c73fbb1d8
Alexandr <sashabratko89@gmail.com>
2020-02-03 19:26:41 +0200

    Add glopt_project

--- dist/js/script.js
+++ dist/js/script.js
@@ -17,4 +17,67 @@ $(document).ready(function() {
       $(".nav__menu").toggleClass("nav__menu_active");
     });
   });
+
+  // слайдер
+  $(".reviews__item").slick({
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 1200,
+    prevArrow: `<button type="button" class="slick-prev">
+		<img src="icons/prev.svg">
+		</button>`,
+    nextArrow: `<button type="button" class="slick-next">
+		<img src="icons/next.svg">
+		</button>`,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          dots: false,
+          arrows: false
+        }
+      }
+    ]
+  });
+  // валидация формы
+  function validateForms(form) {
+    $(form).validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 2
+        },
+        phone: {
+          required: true
+        },
+        email: {
+          required: true,
+          email: true
+        },
+        textarea: {
+          required: false,
+          maxlength: 50
+        }
+      },
+      messages: {
+        name: {
+          required: "Пожалуйста введите ваше имя",
+          minlength: jQuery.validator.format("Введите {0} символа!")
+        },
+        phone: {
+          required: "Пожалуйста введите ваш номер телефона"
+        },
+        email: {
+          required: "Пожалуйста укажите вашу почту",
+          email: "Ваш почтовый ящик должен иметь формат name@domain.com"
+        },
+        textarea: {
+          required: "Введите пожалуйста ваше сообщение"
+        }
+      }
+    });
+  }
+  validateForms("#consultation-form form");
+  validateForms("#question-form form");
 });
